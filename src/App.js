d9859dd6c0bbc31caf1dbffe918ed8e236b6d1e2
Irvin <operationfjh@gmail.com>
2022-10-17 21:13:32 -0700

    Initial nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Nav from './Components/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Nav />
       </header>
     </div>
   );
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import './Nav.css';
+
+function Nav() {
+    return (
+        <nav>
+            <a><div><h3 className='nav-text'>Home</h3></div></a>
+            <a><div><h3 className='nav-text'>New RO</h3></div></a>
+            <a><div><h3 className='nav-text'>Hours</h3></div></a>
+            <a><div><h3 className='nav-text'>Analytics</h3></div></a>
+        </nav>
+    )
+}
+
+export default Nav;
